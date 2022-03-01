33120855e2070664a7e19e2c2533245a450fa661
Jorge Garrido <jegschl@gmail.com>
2022-03-01 19:34:37 -0300

    auto tables creation

--- includes/class-wp-dosf-activator.php
+++ includes/class-wp-dosf-activator.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Fired during plugin activation
  *
@@ -9,6 +8,8 @@
  * @package    Wp_Dosf
  * @subpackage Wp_Dosf/includes
  */
+
+require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
 /**
  * Fired during plugin activation.
@@ -30,7 +31,36 @@ class Wp_Dosf_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		$tables_initializeds = get_option('jgb-dosf_tables_initialized',false);
+		if(!$tables_initializeds){
+			Wp_Dosf_Activator::initialize_tables();
+		}
+	}
 
+	public static function initialize_tables(){
+		global $wpdb;
+		$tbl_nm_shared_objs = $wpdb->prefix . 'dosf_shared_objs';
+		$charset_collate = $wpdb->get_charset_collate();
+		$isql_initialize_tables = "CREATE TABLE IF NOT EXISTS $tbl_nm_shared_objs (
+			id INT UNSIGNED NOT NULL,
+			title varchar(256) NOT NULL,
+			file_name varchar(256) NOT NULL,
+			wp_file_obj_id INT UNSIGNED NULL,
+			created DATETIME NOT NULL
+		) $charset_collate";
+		$wpdb->query( $isql_initialize_tables );
+
+		$isql_initialize_tables = "CREATE UNIQUE INDEX ".$tbl_nm_shared_objs."_id_IDX USING BTREE ON $tbl_nm_shared_objs (id)";
+		$wpdb->query( $isql_initialize_tables );
+
+		$isql_initialize_tables = "CREATE INDEX ".$tbl_nm_shared_objs."_title_IDX USING BTREE ON $tbl_nm_shared_objs (title,id)";
+		$wpdb->query( $isql_initialize_tables );
+
+		$isql_initialize_tables = "CREATE INDEX ".$tbl_nm_shared_objs."_file_name_IDX USING BTREE ON $tbl_nm_shared_objs (file_name,id)";
+		$wpdb->query( $isql_initialize_tables );
+
+		$isql_initialize_tables = "ALTER TABLE $tbl_nm_shared_objs MODIFY COLUMN id int unsigned auto_increment NOT NULL";
+		$wpdb->query( $isql_initialize_tables );
 	}
 
 }
