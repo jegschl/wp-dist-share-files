e5756247bd623981b49d74386121c0647153a382
Jorge Garrido <jegschl@gmail.com>
2022-03-02 15:55:51 -0300

    Funcionalidad visual en el admin

--- admin/js/wp-dosf-admin.js
+++ admin/js/wp-dosf-admin.js
@@ -32,54 +32,26 @@
 	(function( $ ) {
 	'use strict';
 	$(document).ready(function ($) {
-		$('#addPil').click(function(){
-			
-			let html_row_pil = '';
-			let row_id = $('#rows_pils tr').length;
-			
-			$('#rows_pils tr').each(function(i){
-				$(this).find('.img_url_fld_wrapper label').prop('for','img_url[' + i + ']');
-				$(this).find('.ads_url_fld_wrapper label').prop('for','ads_url[' + i + ']');
-			});
-			
-			html_row_pil += '<tr><td><input type="checkbox" name="pil_selection"></td>';
-			html_row_pil += '<td>';
-			html_row_pil += '<div class="img_url_fld_wrapper">';
-			html_row_pil += '<label ';
-			html_row_pil += 'for="img_url[' + row_id + ']"';
-			html_row_pil += '>Url de la Imagen</label>';
-			html_row_pil += '<input type="text" ';
-			html_row_pil += 'name="img_url[' + row_id + ']">';
-			html_row_pil += '</div>';
-			html_row_pil += '<div class="ads_url_fld_wrapper">';
-			html_row_pil += '<label ';
-			html_row_pil += 'for="ads_url[' + row_id + ']"';	
-			html_row_pil +=  '>Url del Ads </label>';
-			html_row_pil +=  '<input type="text" ';
-			html_row_pil +=  'name="ads_url[' + row_id + ']">';
-			html_row_pil +=  '</div>';
-			html_row_pil +=  '</td>';
-			html_row_pil +=  '<td class="col-action-wrapper">';
-			html_row_pil +=  '<div class="action-wrapper remCurrentPil"><i class="fas fa-minus-circle"></i></div>';
-			html_row_pil +=  '</td></tr>';
-			
-			$('#rows_pils').append(html_row_pil);
-			
-			$('.remCurrentPil').off('click');
-			$('.remCurrentPil').click(function(){
-				$(this).parent().parent().remove();
-			});
-		});
-		
-		$('#remPil').click(function(){
-			
-			$('input[name="pil_selection"]:checked').parent().parent().remove();
-		});
-		
-		console.log('===== DEfiniendo evento click de objetos con clase remCurrentPil. ===== ');
-		$('.remCurrentPil').click(function(){
-			$(this).parent().parent().remove();
-		});				
+		dttbl = $('#tabla').DataTable( {
+			processing: true,
+			serverSide: true,
+			ajax: dosf_config.urlGetSOs,
+			language: {
+				url: 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es-cl.json'
+			},
+			columns: [
+				
+				{
+					data: 'title'
+				},
+				{
+					data: 'file_name'
+				},
+				{
+					data: 'ruts_linked'
+				}
+			]
+		} );			
 	});
 	
 })( jQuery );
